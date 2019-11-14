8442c589371205f805b0da328127345c5de53019
Matthias Michael Loker <m@theloker.com>
2019-11-14 16:29:18 +0100

    Add dropdown menu

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
   config.addPassthroughCopy("site/images"); // pass images folder right through to output folder
   config.addPassthroughCopy("site/admin"); // pass admin folder right through to output folder
+  config.addPassthroughCopy("site/js"); // pass js folder right through to output folder
   return {
     dir: {
       input: "site",
