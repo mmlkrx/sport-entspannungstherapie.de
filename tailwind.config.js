5c3efcf10dc04cf6d767f1a0ffa6c26947450f74
Matthias Michael Loker <m@theloker.com>
2019-11-26 13:09:37 +0100

    Add copyright notice to footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
         'beige-200': '#f5ede5',
         'beige-300': '#eae0d6',
         'beige-400': '#e2d3c3',
-        'beige-500': '#d0c0af'
+        'beige-500': '#d0c0af',
+        'beige-600': '#ad9d8c'
       }
     }
   }
